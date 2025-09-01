98e53dbf0ed054feec2fb97f0d4098bd54085fab
Bharti <bhartiwadhwa025@gmail.com>
2025-09-01 17:33:19 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,26 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+   const projects = [
+    {
+      name: "AI Healthcare Assistant",
+      description: "An AI-powered assistant to help doctors with diagnosis and treatment recommendations."
+    },
+    {
+      name: "Smart Farming App",
+      description: "A mobile app that uses IoT and machine learning to optimize agricultural productivity."
+    }
+  ];
+
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+      {projects.map((project,index)=>(
+        <div>
+      <h1 data-ns-test="project-name">{project.name}</h1>
+      <h6 data-ns-test="project-description">{project.description}</h6>
+      </div>
+      ))}
+    </div>
   )
 }
 
